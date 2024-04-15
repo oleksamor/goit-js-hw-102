e8fbefbd4b3b3c7c4e3763933283a47b53982284
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-04-15 13:33:00 +0300

    add flatpickr

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,6 +1,20 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const fieldDatatime = document.getElementById("datetime-picker");
+console.log(fieldDatatime);
+
+const startBtn = document.querySelector("button[data-start]");
+console.log(startBtn);
+
+fieldDatatime.addEventListener("click", onClickDatatime);
+
+function onClickDatatime(event) {
+  console.log(event);
+}
+
+let userSelectedDate;
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -8,5 +22,19 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    userSelectedDate = selectedDates[0];
+    if (userSelectedDate < Date.now()) {
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+  
   },
-};
+}
+
+const fp = flatpickr(fieldDatatime, 
+);
